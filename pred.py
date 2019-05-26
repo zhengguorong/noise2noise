eeb033ba0d9feac30b51db569872b00e5c1cc412
郑国榕 <zhengguorong@bluemoon.com.cn>
2019-05-26 13:41:08 +0800

    chagne

--- pred.py
+++ pred.py
@@ -49,9 +49,12 @@ def main():
         h, w, _ = image.shape
 
         out_image = np.zeros((h, w * 3, 3), dtype=np.uint8)
+        noise_image = val_noise_model(image)
         pred = model.predict(np.expand_dims(image, 0))
         denoised_image = get_image(pred[0])
-        out_image[:, :w] = denoised_image
+        out_image[:, :w] = image
+        out_image[:, w:w * 2] = noise_image
+        out_image[:, w * 2:] = denoised_image
 
         if args.output_dir:
             cv2.imwrite(str(output_dir.joinpath(image_path.name))[:-4] + ".png", out_image)
